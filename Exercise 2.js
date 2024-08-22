12cd088fe3be605eb56f0e4523dc4da973767df7
Richard <richardmusime6@gmail.com>
2024-08-22 08:47:36 +0200

    Refactored the function by making it arrow function and also using template literals for concetination and ternary operator for checking if we have name

--- Exercise 2.js
+++ Exercise 2.js
@@ -1,7 +1,1 @@
-function greet(name) {
-    if (name) {
-      return "Hello, " + name + "!";
-    } else {
-      return "Hello!";
-    }
-  }
+const greet = name => name ? `Hello, ${name}!` : "Hello!";
