f4ae799472c3a293e06873f0b2903da488a0f895
Richard <richardmusime6@gmail.com>
2024-08-22 09:46:54 +0200

    Refactored the function by making it arrow function an also changed var to be let keyword

--- Exercise 9.js
+++ Exercise 9.js
@@ -1,7 +1,7 @@
-function calculateMatrixSum(matrix) {
-    var sum = 0;
-    for (var i = 0; i < matrix.length; i++)
-      for (var j = 0; j < matrix[i].length; j++)
-        sum += matrix[i][j];
-    return sum;
-  }
+const calculateMatrixSum = matrix => {
+    let sum = 0;
+    for (let i = 0; i < matrix.length; i++){
+        for (let j = 0; j < matrix[i].length; j++){
+         sum += matrix[i][j]; }}    
+    return sum; 
+}
