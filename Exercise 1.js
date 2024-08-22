a31a071fad8289008b8b8a26c45aa01944046a6f
Richard <richardmusime6@gmail.com>
2024-08-22 08:42:14 +0200

    Added a new line on the returned solution

--- Exercise 1.js
+++ Exercise 1.js
@@ -1,1 +1,2 @@
-const calculateAverage = numbers => numbers.length === 0 ? 0 : numbers.reduce((sum,number)=> sum + number, 0) / numbers.length;
+const calculateAverage = numbers => 
+numbers.length === 0 ? 0 : numbers.reduce((sum,number)=> sum + number, 0) / numbers.length;
