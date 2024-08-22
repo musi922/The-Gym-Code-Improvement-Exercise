fad721bb7837f2fd636e949b6eae1ce504ad360b
Richard <richardmusime6@gmail.com>
2024-08-22 08:56:44 +0200

    Refactored a function by using arrow functions and use es6 methods such as split, reverse and join method for simplicity of fucntion

--- Exercise 3.js
+++ Exercise 3.js
@@ -1,7 +1,1 @@
-function reverseString(str) {
-    var reversed = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-      reversed += str[i];
-    }
-    return reversed;
-  }
+const reverseString = inputString => inputString.split("").reverse().join("");
