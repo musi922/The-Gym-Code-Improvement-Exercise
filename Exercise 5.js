edb092b83c901d911aad2a21b67bc1520991ab0e
Richard <richardmusime6@gmail.com>
2024-08-22 09:13:58 +0200

    Refactored the function using arrowfunction and used the ternary operator for checking if is less than 1 and also used recursive function to solve the factorial

--- Exercise 5.js
+++ Exercise 5.js
@@ -1,11 +1,1 @@
-function calculateFactorial(n) {
-    if (n === 0 || n === 1) {
-      return 1;
-    } else {
-      var factorial = 1;
-      for (var i = 2; i <= n; i++) {
-        factorial *= i;
-      }
-      return factorial;
-    }
-  }
+const calculateFactorial = number => number <= 1 ? 1 : number * calculateFactorial(number-1);
